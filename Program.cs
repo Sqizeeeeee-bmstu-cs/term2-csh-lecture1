5a609e0f7c6812f4902c3e1b987e26e461fa39f9
vladimir <vladimir.oleynyk057@gmail.com>
2026-02-13 21:05:47 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// #TODO: task 2
+
+// string input = Console.ReadLine()!;
+// int number;
+// bool res = int.TryParse(input, out number);
+
+
+// if (res == true)
+// {
+//     Console.WriteLine($"{number} is a number");
+// }
+// else
+// {
+//     Console.WriteLine($"{input} is not a number, it is a string");
+// }
+
+
+
+// #TODO: task 3
+
+// for (int i = 0; i < 10; i++)
+//     if (i % 2 == 0)
+//         Console.Write($"{i} ");
+
+// Console.WriteLine();
+
+
