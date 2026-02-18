4e57c886844bcbe2fafde3ae4beabed38a83cb08
vladimir <vladimir.oleynyk057@gmail.com>
2026-02-18 21:39:32 +0300

    added task 4

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-// #TODO: task 2
+// TODO: task 2
 
 // string input = Console.ReadLine()!;
 // int number;
@@ -19,7 +19,7 @@
 
 
 
-// #TODO: task 3
+// TODO: task 3
 
 // for (int i = 0; i < 10; i++)
 //     if (i % 2 == 0)
@@ -27,4 +27,106 @@
 
 // Console.WriteLine();
 
+
+// TODO: task 4
+
+// static void AnalyzeWithIf(object?[] data)
+// {
+//     foreach(object? obj in data)
+//     {
+//         if(obj is null)
+//         {
+//             Console.WriteLine("object is null");
+//         }
+
+//         else if (obj is int i1)
+//         {
+//             if (i1 > 0)
+//             {
+//                 Console.WriteLine($"{obj} is a positive number");
+//             }
+
+//             else if (i1 < 0)
+//             {
+//                 Console.WriteLine($"{obj} is a negative number");
+//             }
+            
+//             else
+//             {
+//                 Console.WriteLine($"{obj} is a zero");
+//             }
+//         }
+
+//         else if(obj is string s1)
+//         {
+//             Console.WriteLine($"{obj} is a string");
+//         }
+
+//         else
+//         {
+//             Console.WriteLine("idk what object is");
+//         }
+//     }
+// }
+
+
+// static void AnalyzeWithSwitch(object?[] data)
+// {
+//     foreach (object? obj in data)
+//     {
+//         switch (obj)
+//         {
+//             case null:
+//                 Console.WriteLine("object is null");
+//                 break;
+
+//             case string s1:
+//                 Console.WriteLine($"{obj} is a string");
+//                 break;
+
+//             case int i1:
+//                 if (i1 > 0)
+//                 {
+//                     Console.WriteLine($"{obj} is a positive number");
+//                 }
+
+//                 else if (i1 < 0)
+//                 {
+//                     Console.WriteLine($"{obj} is a negative number");
+//                 }
+                
+//                 else
+//                 {
+//                     Console.WriteLine($"{obj} is a zero");
+//                 }
+
+//                 break;
+
+//             default:
+//                 Console.WriteLine("idk what object is");
+//                 break;
+//         }
+//     }
+// }
+
+// static string GetCategory(object? obj) => obj switch
+// {
+//     int i when i > 0 => "Positive number",
+//     int i when i < 0 => "Negative number",
+//     int i => "Zero",
+//     string s => "String",
+//     null => "Null",
+//     _ => "Unknown"
+// };
+
+// object?[] data = { 15, "Hello", -5, null,  "World", 42, 0, "C#", -10 };
+
+// AnalyzeWithIf(data);
+// Console.WriteLine("------------------------------");
+// AnalyzeWithSwitch(data);
+// Console.WriteLine("------------------------------");
+// foreach (var item in data)
+// {
+//     Console.WriteLine($"{item ?? "null"} - {GetCategory(item)}");
+// }
 
