420ee1506f25ea2a2624d7d012121ad3a1f04c16
vladimir <vladimir.oleynyk057@gmail.com>
2026-02-27 16:56:02 +0300

    added main task

--- Program.cs
+++ Program.cs
@@ -28,6 +28,52 @@
 // Console.WriteLine();
 
 
+// TODO: task 3.5
+
+
+
+// static void calcroots(int a, int b, int c)
+// {
+//     double d = Math.Sqrt(b * b - 4 * a * c);
+//     double temp = ((-1 * b) + d) / (2 * a);
+//     double x1 = Math.Sqrt(temp);
+//     double x2 = x1 * (-1);
+
+//     double tempp = ((-1 * b) - d) / (2 * a);
+//     double x3 = Math.Sqrt(tempp);
+//     double x4 = x3 * (-1);
+
+//     double[] arr =  new double[4];
+    
+//     arr[0] = x1;
+//     arr[1] = x2;
+//     arr[2] = x3;
+//     arr[3] = x4;
+
+//     for (int i = 0; i < 4; i++)
+//     {
+//         for (int j = 0; j < i + 1; j++)
+//         {
+//             if (arr[j] > arr[i])
+//             {
+//                 double p = arr[i];
+//                 arr[i] = arr[j];
+//                 arr[j] = p;
+//             }
+//         }
+//     }
+
+//     foreach (var item in arr)
+//     {
+//         Console.WriteLine(item);
+//     }
+// }
+
+// int a = 1;
+// int b = -5;
+// int c = 4;
+// calcroots(a,b,c);
+
 // TODO: task 4
 
 // static void AnalyzeWithIf(object?[] data)
